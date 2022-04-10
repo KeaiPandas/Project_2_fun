98efd9a742d1f25afaaea7dbdc9c05d5133a9417
KeaiPandas <zkphao3@gmail.com>
2022-04-10 11:58:03 +0800

    update

--- vue-manage/src/components/CommonAside.vue
+++ vue-manage/src/components/CommonAside.vue
@@ -6,6 +6,10 @@
     @close="handleClose"
     :collapse="isCollapse"
   >
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">导航二</span>
+    </el-menu-item>
     <el-submenu index="1">
       <template slot="title">
         <i class="el-icon-location"></i>
@@ -14,35 +18,15 @@
       <el-menu-item-group>
         <span slot="title">分组一</span>
         <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
     </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
   </el-menu>
 </template>
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
-  min-height: 400px
+  min-height: 400px;
 }
 </style>
 
@@ -50,8 +34,51 @@
 export default {
   data() {
     return {
-      isCollapse: true,
-    }
+      isCollapse: false,
+      menu: [
+        {
+          path:'/',
+          name: 'home',
+          label: '首页',
+          icon: 's-home',
+          url: 'Home/Home'
+        },
+        {
+          path:'/mail',
+          name: 'mail',
+          label: '商品管理',
+          icon: 'Video-play',
+          url: 'MailManage/MailManage'
+        },
+        {
+          path:'/user',
+          name: 'user',
+          label: '用户管理',
+          icon: 'user',
+          url: 'UserManage/UserManage'
+        },
+        {
+          label: '其他',
+          icon: 'location',
+          children: [
+            {
+              path: '.page1',
+              name: 'page1',
+              label: '页面1',
+              icon: 'setting',
+              url: 'Other/PageOne'
+            },
+            {
+              path: '/page2',
+              name: 'page2',
+              label: '页面2',
+              icon: 'setting',
+              url: 'Other/PageTwo'
+            }
+          ]
+        }
+      ]
+    };
   },
   methods: {
     handleOpen(key, keyPath) {
